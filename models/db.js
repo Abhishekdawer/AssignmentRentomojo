2bd495702bc2176f5d40a3ad6137d489a6cdea65
Abhishekdawer <abhishekdawer045@gmail.com>
2020-05-31 12:20:37 +0530

    deploy

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
- //mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => { // for local DB
-mongoose.connect('mongodb+srv://dawar:dawar@cluster0-8gafo.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+ mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => { // for local DB
+//mongoose.connect('mongodb+srv://dawar:dawar@cluster0-8gafo.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
         if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
